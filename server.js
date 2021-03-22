afc5109546429f85a5ad72ed7cf889d0388923fc
Jenn Greiner <jenn.greiner1@gmail.com>
2021-03-21 21:49:35 -0600

    commented server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// The application should have a `db.json` file on the back end that will be used to store and retrieve notes using the `fs` module.
+// Global variables and required packages
 const express = require('express');
 const path = require('path')
 const fs = require('fs');
@@ -9,11 +9,11 @@ const { v4: uuidv4 } = require('uuid');
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Enables css and js to link on html files in public folder
 app.use(express.static('public'));
 
-
-// The following HTML routes should be created:
-// create path to dbjson
+// HTTP Routes
+// create path to dbjs.on on the back end that will be used to store and retrieve notes using the `fs` module
 const dbPath = path.join(__dirname, 'db/db.json')
 
 // GET requests
@@ -25,13 +25,13 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 
 // `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
-    // read the db.json file
+    // Read the db.json file
     let noteArray = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
-    // assign an id value to each item in noteArray
+    // Assign an id value to each item in noteArray
     noteArray.forEach((note, index) => {
         note.id = index + 1;
     });
-    //return saved notes as json
+    // Return saved notes as json
     return res.json(noteArray);
 });
 
@@ -45,8 +45,9 @@ app.post('/api/notes', (req, res) => {
     // Convert string into JSON object
     let allNotes = JSON.parse(existingNotes);
 
-let newNote = req.body;
-newNote.id = uuidv4();
+    let newNote = req.body;
+    // Assign a unique ID to each note in db.json using the uuid package
+    newNote.id = uuidv4();
     // Add the new note to the existingNotes array
     allNotes.push(newNote);
     console.log(allNotes);
@@ -55,13 +56,6 @@ newNote.id = uuidv4();
     res.json(req.body);
 });
 
-// app.put('/api/notes/:id', (req, res) => {
-//     const { author, value } = req.body;
-//     // change a note --- which note?
-//     // re.params.id (/notes/:id)
-//     // req.query.id (/notes?id=)
-//     // no identifier changes all the notes
-// });
 
 // DELETE request
 app.delete('/api/notes/:id', (req, res) => {
